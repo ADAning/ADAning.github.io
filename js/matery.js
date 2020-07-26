f6a91db6cec6d12c7abb5734ff459d2d76fa6c9c
ADAning <37218117+ADAning@users.noreply.github.com>
2020-07-26 18:34:15 +0800

    Site updated: 2020-07-26 18:33:38

--- js/matery.js
+++ js/matery.js
@@ -106,6 +106,10 @@ $(function () {
             subHtmlSelectorRelative: true
         });
 
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
+        
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
